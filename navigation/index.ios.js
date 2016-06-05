7483cb9f1d68bef6ef99d0517d7c65a4929c898f
Gabriel Firmacion <gabrielfirmacion@gmail.com>
2016-06-05 10:10:25 -0700

    setup environment and have hello world

---
+++ navigation/index.ios.js
@@ -0,0 +1,29 @@
+var React = require('react');
+var ReactNative = require('react-native');
+var {
+  Text,
+  View,
+  AppRegistry,
+  StyleSheet
+} = ReactNative;
+
+var Navigation = React.createClass({
+  render: function() {
+    return <View style={styles.container}>
+      <Text style={styles.text}>Hello World!</Text>
+    </View>
+  }
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  text: {
+    fontSize: 30
+  }
+});
+
+AppRegistry.registerComponent('navigation', () => Navigation);
